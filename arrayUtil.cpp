b6a2a085bf25fd4f2c12c7aeaae1c7afc69dd589
tina445 <gmltjq4731@icloud.com>
2024-08-30 16:09:52 +0900

    arrutil 기능 구현 완료(int - weak)

--- arrayUtil.cpp
+++ arrayUtil.cpp
@@ -8,7 +8,7 @@ int input_num;
 vector<int> arr1;
 vector<int> arr2;
 
-vector<int> first_fun(vector<int> &vec){
+vector<int> arrUnion(vector<int> &vec){
     vector<int> cnt(10, 0);
     vector<int> ret;
 
@@ -24,6 +24,38 @@ vector<int> first_fun(vector<int> &vec){
     return ret;
 }
 
+vector<int> arrSum(vector<int> &vec1, vector<int> &vec2) {
+    vector<int> cnt1(10, 0), cnt2(10, 0);
+    vector<int> ret;
+
+    for (int i = 0; i < 10; i++){
+        cnt1[vec1[i]]++;
+        cnt2[vec2[i]]++;
+    }
+
+    for (int i = 0; i < 10; i++){
+        if (cnt1[i] || cnt2[i]) ret.emplace_back(i);
+    }
+
+    return ret;
+}
+
+vector<int> arrProduct(vector<int> &vec1, vector<int> &vec2) {
+    vector<int> cnt1(10, 0), cnt2(10, 0);
+    vector<int> ret;
+
+    for (int i = 0; i < 10; i++){
+        cnt1[vec1[i]]++;
+        cnt2[vec2[i]]++;
+    }
+
+    for (int i = 0; i < 10; i++){
+        if (cnt1[i] && cnt2[i]) ret.emplace_back(i);
+    }
+
+    return ret;
+}
+
 
 int main(){
     int x;
@@ -32,10 +64,24 @@ int main(){
         cin >> x;
         arr1.emplace_back(x);
     }
+
+    for (int i = 0; i < 10; i++) {
+        cin >> x;
+        arr2.emplace_back(x);
+    }
     
 
-    vector<int> res = first_fun(arr1);
+    vector<int> res = arrUnion(arr1);
 
     for (auto e : res) cout << e << " ";
+    cout << "\n";
 
+    res = arrSum(arr1, arr2);
+
+    for (auto e : res) cout << e << " ";
+    cout << "\n";
+
+    res = arrProduct(arr1, arr2);
+    for (auto e : res) cout << e << " ";
+    cout << "\n";
 }
