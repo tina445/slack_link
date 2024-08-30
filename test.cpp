3a668aeef4b4b672d1bfb3523dbfcc99ac2784e9
tina445 <gmltjq4731@icloud.com>
2024-08-30 15:19:52 +0900

    숫자 콤마 찍기 complete

--- test.cpp
+++ test.cpp
@@ -1,12 +1,73 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main() {
-    for (int i = 2; i <= 9; i++) {
-        for (int j = 1; j <= 9; j++) {
-            cout << i << " * " << j << " = " << i*j << "\t";
-        }
-        cout << "\n";
+bool digit(string str) {
+    for (int i = 0; i < str.length(); i++) {
+        if (!isdigit(str[i])) return false;
     }
+
+    return true;
+}
+
+string make_str(string str){
+
+    string ret  = "";
+    int cnt{};
+
+
+
+    return ret;
+
+}
+
+string make_str3(string str){
+
+    string ret  = "";
+    int cnt{};
+
+    for (int i = 0; i < str.size()-1; i++) {
+        ret += str[i];
+        if(++cnt % 3 == 0) ret ;
+    }
+
+    ret += str[str.size()-1];
+
+    return ret;
+
+}
+
+int main() {
+    string str, ret{}, sig; 
+    int err{};
+    
+    while (1) {
+        if (err > 2) {
+            ret = "error";
+            break;
+        }
+
+        cin >> str;
+        int size{str.length()};
+        if (str[0] == '-') {
+            sig = '-';
+            str.erase(str.begin());
+        }
+        
+        if (!digit(str) || size >= 20) {
+            cout << "다시 입력해주세요.\n";
+            err++;
+            continue;
+        }
+    
+    int cnt{};
+    for (int i = str.length()-1; i >= 0; i--) {
+        ret = str[i] + ret;
+        if(++cnt % 3 == 0 && i != 0) ret = ',' + ret;
+    }
+        break;
+    }
+
+    cout << sig + ret << "\n";
 }
